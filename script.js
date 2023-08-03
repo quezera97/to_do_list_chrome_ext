401af3750eab3a962354d8ae1db16ab96fb3f027
LoleeconDestroyer <66286710+quezera97@users.noreply.github.com>
2023-08-03 12:11:53 +0800

    setting assets

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const getTimeStamp = () => {
     const formattedTimeStamp = `${formattedDate} | ${currentTime}`;
   
     return formattedTimeStamp;
-  }
+};
   
 const swap = (i, j) => {
 
@@ -36,13 +36,11 @@ const swap = (i, j) => {
         return;
     else if(i >= taskList.length || j >= taskList.length)
         return;
+
     [taskList[i], taskList[j]] = [taskList[j], taskList[i]];
-}
+};
 
-
-/**------function to load the view-------*/
 const loadView = () => {
-
     //first clear .task-list by removing all child under child-list div
     const taskListDiv = document.getElementById("task-list");
 
@@ -59,11 +57,8 @@ const loadView = () => {
 
     taskList.forEach((Element,index) =>{
 
-
         const taskBox = document.createElement("div");
         taskBox.setAttribute("class", Element.isDone ? "task-box done" : "task-box");
-
-
 
         // the task text
         const taskText = document.createElement("div");
@@ -79,12 +74,9 @@ const loadView = () => {
         taskText.innerHTML = "<strong>" + (index+1) + "</strong>" + ". " + task;
         taskBox.appendChild(taskText);
 
-
-
         // task controller - for buttons and timestamp
         const taskController = document.createElement("div");
         taskController.setAttribute("class", "task-controller");
-
 
         // add the timestamp(the time of adding new task)
         const timeStamp = document.createElement("span");
@@ -95,12 +87,9 @@ const loadView = () => {
         updatedTimeStamp.setAttribute("class", Element.isDone ? "timestamp done" : "timestamp");
         updatedTimeStamp.innerText = (Element.updatedAt == null) ? "" : ("[Updated: " + Element.updatedAt + "]");
 
-
         //controller button group
         const controllerBtns = document.createElement("span");
         controllerBtns.setAttribute("class", "controller-btn");
-        
-
         
         //button to mark done or to undo task 
         const doneBtn = document.createElement('button');
@@ -117,15 +106,12 @@ const loadView = () => {
         })
         controllerBtns.appendChild(doneBtn);
 
-
-
         //move up button
         const moveUpBtn = document.createElement('button');
         moveUpBtn.className = 'move-up-btn';
         moveUpBtn.textContent = '↑';
         moveUpBtn.addEventListener("click", () => {
-            if(taskList[index].isDone)
-            {
+            if(taskList[index].isDone){
                 alert("The task is completed. Completed task cannot be moved!");
                 return;
             }
@@ -136,14 +122,12 @@ const loadView = () => {
         });
         controllerBtns.appendChild(moveUpBtn);
 
-
         //move down button
         const moveDownBtn = document.createElement('button');
         moveDownBtn.className = 'move-down-btn';
         moveDownBtn.textContent = '↓';
         moveDownBtn.addEventListener("click", () => {
-            if(taskList[index].isDone)
-            {
+            if(taskList[index].isDone){
                 alert("The task is completed. Completed task cannot be moved!");
                 return;
             }
@@ -154,16 +138,13 @@ const loadView = () => {
         });
         controllerBtns.appendChild(moveDownBtn);
 
-
         //button for editing task
         const editTaskBtn = document.createElement('button');
         editTaskBtn.className = 'edit-task-btn';
         editTaskBtn.textContent = 'Edit';
         
         editTaskBtn.addEventListener("click",() => {
-           
-            if(taskList[index].isDone)
-            {
+            if(taskList[index].isDone){
                 alert("The task is completed. Completed task cannot be updated!");
                 return;
             }
@@ -175,18 +156,14 @@ const loadView = () => {
         })
         controllerBtns.appendChild(editTaskBtn);
 
-
-
         //button for deleting task
         const delTaskBtn = document.createElement('button');
         delTaskBtn.className = 'del-task-btn';
         delTaskBtn.textContent = 'Delete';
 
         delTaskBtn.addEventListener("click", () => {
-
-            let x = prompt("Are you sure? This can't be undone!\nWrite 1 to delete.");
-            if(x == 1)
-            {
+            let x = prompt("Are you sure? This can't be undone!\n\nWrite 1 to delete.");
+            if(x == 1){
                 taskList.splice(index, 1);
                 localStorage.setItem("todo", JSON.stringify(taskList));
                 loadView();
@@ -194,58 +171,43 @@ const loadView = () => {
         })
         controllerBtns.appendChild(delTaskBtn);
 
-
         // append childs - timestamp,controllerBtns->task-controller->taskbox
         taskController.appendChild(timeStamp);
 
         if(!taskList[index].isDone)
             taskController.appendChild(updatedTimeStamp);
 
-         // add completion time for tasks that are done
-         if(taskList[index].isDone)
-         {
-             const completionTime = document.createElement("div");
-             completionTime.setAttribute("class","timeStamp");
-             completionTime.innerText = "[Completed: " + taskList[index].completionTime + "]";
-             taskController.appendChild(completionTime);
-         }
+        // add completion time for tasks that are done
+        if(taskList[index].isDone){
+            const completionTime = document.createElement("div");
+            completionTime.setAttribute("class","timeStamp");
+            completionTime.innerText = "[Completed: " + taskList[index].completionTime + "]";
+            taskController.appendChild(completionTime);
+        }
          
         taskController.appendChild(controllerBtns);
         taskBox.appendChild(taskController);
 
         // append everything to task-list div
         document.getElementById("task-list").appendChild(taskBox);
-
-
-
-        
-        
-    })
-}
-
-
-
-// function to Toggle Task Input Form
+    });
+};
 
 const toggleToDoForm = () => {
-
     const todoForm = document.getElementById('todo-input-form');
-
-    if(todoForm.style.display == 'block')
-    {
+    
+    if(todoForm.style.display == 'block'){
         todoForm.style.display = 'none';
         addToDoFormBtn.innerText = '+';  
         addToDoFormBtn.style.padding = "5px 10px"; 
 
     }
-    else
-    {
+    else{
         todoForm.style.display = 'block';
         addToDoFormBtn.innerText = '-';
         addToDoFormBtn.style.padding = "5px 13px"; 
     }
-}
-
+};
 
 /**
  * function to Add New Task in todo list
@@ -258,8 +220,7 @@ const addNewTask = (priority, task, isDone) => {
    
     toggleToDoForm();
 
-    if(task === null || task.trim() === "")
-    {
+    if(task === null || task.trim() === "" ){
         alert("Task field is empty!");
         return;
     }
@@ -274,27 +235,18 @@ const addNewTask = (priority, task, isDone) => {
     localStorage.setItem("todo", JSON.stringify(taskList));
 
     loadView();
-}
-
-
- 
-
-
-/**======== add Event Listeners to buttons and divs ======== */
+};
 
 addToDoFormBtn.addEventListener("click", toggleToDoForm);
 
 closeBtn.addEventListener("click", () => {
     modal.style.display = 'none';
-})
+});
 
-
-// add event listener to update task button in modal
 updateTaskBtn.addEventListener("click", () => {
 
-    let x = prompt("Are you sure to edit? This can't be undone!\nWrite 1 to edit.");
-    if(x == 1)
-    {
+    let x = prompt("Are you sure to edit? This can't be undone!\n\nWrite 1 to edit.");
+    if(x == 1){
         const ind = document.getElementById('hidden-index-updated').value;
         const updatedTask = document.getElementById('task-input-updated').value;
 
@@ -306,36 +258,31 @@ updateTaskBtn.addEventListener("click", () => {
 
     modal.style.display = 'none';
     loadView();
+});
 
-})
-
-
-// button to add high priority task
 addTaskBtnHigh.addEventListener("click", () => {
 
     const taskHigh = document.getElementById('task-input-high').value;
+
     document.getElementById('task-input-high').value = "";
-    addNewTask("high",taskHigh,false);
 
-})
+    addNewTask("high", taskHigh, false);
+});
 
-//button to add low priority task
 addTaskBtnLow.addEventListener("click", () => {
 
     const taskLow = document.getElementById('task-input-low').value;
+
     document.getElementById('task-input-low').value = "";
+
     addNewTask("low", taskLow, false);
-
-})
-
-
+});
 
 document.addEventListener("DOMContentLoaded", () => {
     const toDoList = JSON.parse(localStorage.getItem("todo"));
+
     if(toDoList !== null)
         taskList = [...toDoList];
+
     loadView();
-
-})
-
-/**------------------------------------------ */
+});
